0ddfa848186b26e947721215800646192dcd405d
Peter Tumulty <ptumulty923@gmail.com>
2021-11-23 11:36:43 -0500

    added first round of tests

---
+++ components/Header/__tests__/Header.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+import React from 'react'
+
+import { renderWithTheme } from '../../test-util'
+import Header from '..'
+
+describe('<Header />', () => {
+  it('renders name', () => {
+    renderWithTheme(<Header />)
+
+    expect(screen.queryByText('Unleashed Potential: Life Coaching')).toBeInTheDocument()
+  })
+
+  it('renders menu component', () => {
+    renderWithTheme(<Header />)
+
+    expect(screen.queryByText('Our Story')).toBeInTheDocument()
+  })
+})
---
+++ components/Header/__tests__/Menu.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+import React from 'react'
+
+import { renderWithTheme } from '../../test-util'
+import Menu from '../Menu'
+
+describe('<Menu />', () => {
+  it('renders menu item', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText('Our Story')).toBeInTheDocument()
+  })
+
+  it('does not render malformed item', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText('Our Story!')).toBeNull()
+  })
+})
